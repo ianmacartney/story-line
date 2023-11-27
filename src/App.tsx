a76836913af0fda470a15591c15ee70b617aeb2a
Ian Macartney <ianhmacartney@gmail.com>
2023-11-27 14:38:57 -0700

    add UI to watch

--- src/App.tsx
+++ src/App.tsx
@@ -2,28 +2,20 @@ import { Button } from "@/components/ui/button";
 import { useMutation, useQuery } from "convex/react";
 import { api } from "../convex/_generated/api";
 import { useState } from "react";
+import { Input } from "./components/ui/input";
 
 function App() {
   const [code, setCode] = useState("");
   const story = useQuery(api.myFunctions.getStory, code ? { code } : "skip");
-  const start = useMutation(api.myFunctions.start);
+  // const start = useMutation(api.myFunctions.start);
 
   return (
     <main className="container max-w-2xl flex flex-col gap-8">
       <h1 className="text-4xl font-extrabold my-8 text-center">Story Line</h1>
-      {code && <h2>{code}</h2>}
-      <p>
-        <Button
-          onClick={async () => {
-            const phone = prompt("Enter a phone number:");
-            if (phone === null) return;
-            const code = await start({ phone });
-            setCode(code);
-          }}
-        >
-          Add a random number
-        </Button>
-      </p>
+      <h2>
+        Code:
+        <Input onChange={(e) => setCode(e.target.value)} />
+      </h2>
       {story && story.map((item) => <p key={item}>{item}</p>)}
     </main>
   );
